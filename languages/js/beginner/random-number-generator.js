598401c3c0d6e6fda5cf85143aa036ed8821e035
HipsterSantos <santoscampos269@gmail.com>
2021-01-31 06:20:59 +0100

    random number generator , all  begginer stuff to get started with number in javascript

---
+++ languages/js/beginner/random-number-generator.js
@@ -0,0 +1,8 @@
+Math.random() * 100; //this will generate number between 0 and 100
+Math.random() * 25+5;//this will generate number in range of 5 to  30 
+Math.random() * 10 - 100 ;//this will generate number in range of -100 and -90
+//if you wanna get integer value use this code beneath it 
+
+Math.floor(Math.random() * 100 ) 
+Math.floor(Math.random() * 25) + 5;
+Math.floor(Math.random() * 10) - 100
