0f55a4ce51667e7b75dd9d152355567d8d5398cd
HipsterSantos <santoscampos269@gmail.com>
2021-01-31 06:41:57 +0100

    All challenge problem added

---
+++ languages/js/beginner/problem-1.js
@@ -0,0 +1,7 @@
+// Modular exponentiation 
+// Given three numbers x, y, and p, compute (xˆy) % p. (This is
+//     modular exponentiation.)
+//     Here, x is the base, y is exponent, and p is the modulus.
+//     Modular exponentiation is a type of exponentiation performed
+//     over a modulus, which is useful in computer science and used in
+//     the field of public-key encryption algorithms.
---
+++ languages/js/beginner/problem-2.js
@@ -0,0 +1,4 @@
+// Print all primes less than n.
+// To do this, use the isPrime function covered in this chapter.
+// Simply iterate from 0 to n and print any prime numbers where
+// isPrime() evaluates to true.
