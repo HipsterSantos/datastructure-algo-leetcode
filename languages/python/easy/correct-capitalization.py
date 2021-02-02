aebf007e791e9566ad62938d5bed1afae494204e
HipsterSantos <santoscampos269@gmail.com>
2021-02-02 21:49:48 +0100

    New problems added to the set

---
+++ languages/python/easy/correct-capitalization.py
@@ -0,0 +1,10 @@
+asked by Google
+
+Given a string, return whether or not it uses capitalization correctly.
+ A string correctly uses capitalization if all letters are capitalized, no letters are capitalized,
+  or only the first letter is capitalized.
+
+    "USA", return true
+"Calvin", return true
+"compUter", return false
+"coding", return true
