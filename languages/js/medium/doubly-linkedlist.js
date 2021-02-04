64c968148b76b0e2d47ee934c5c0bb7ae56ed009
HipsterSantos <santoscampos269@gmail.com>
2021-02-04 08:23:09 +0100

    Add at tail function created

--- languages/js/medium/doubly-linkedlist.js
+++ languages/js/medium/doubly-linkedlist.js
@@ -12,3 +12,27 @@ function LinkedList(){
 LinkedList.prototype.isEmpty = function(){
   return this.size === 0 ;
 }
+
+LinkedList.prototype.addAtFront = function(value){
+  if(this.head == null){
+    this.head - new DLNode(value);
+    this.tail  = this.head;
+  }else{
+    var temp = new DLNode(value)
+    this.head.next = this.head;
+    this.head.prev = temp;
+    this.head = temp;
+  }
+}
+
+LinkedList.prototype.addAtTail = function(value){
+  if(this.tail === null ){
+    this.tail = new DLNode(value);
+    this.head = this.tail;
+  }else{
+
+    this.tail.prev = this.tail;
+    this.tail.next = temp;
+    this.tail = temp
+  }
+}
