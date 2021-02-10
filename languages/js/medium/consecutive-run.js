f95f9cf36fab603a1882eafd1baeaeabde40fb46
HipsterSantos <santoscampos269@gmail.com>
2021-02-10 06:10:48 +0100

    one more problem added

---
+++ languages/js/medium/consecutive-run.js
@@ -0,0 +1,10 @@
+
+Longest Consecutive Run of 1s in Binary
+Given a non-negative integer n,
+ return the length of the longest consecutive run of 1s in its binary representation.
+ Input
+n = 156
+Output
+3
+Explanation
+156 is10011100 in binary and there's a run of length 3.
