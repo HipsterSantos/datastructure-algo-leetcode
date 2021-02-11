41f57a64f359b1b628fb63f288fb815293d6afb6
HipsterSantos <santoscampos269@gmail.com>
2021-02-11 07:13:42 +0100

    adding content to challenge

--- languages/js/easy/review/challengeREgExp.js
+++ languages/js/easy/review/challengeREgExp.js
@@ -5,3 +5,21 @@ here, just the part that reads the configuration file. Sorry.) The configuration
 file looks like this:
 
 Warning: Content from Eloquent JavaScriptBook p(160)
+
+function parseINI(string){
+  //Start with an object to hold the top-level fields
+  let result = {};
+  let section = resul;
+  string.split(/\r?\n/).forEach(line=>{
+    let match;
+    if(match == line.match(/^(\w+)=(.*)$/)){
+      section[match[1]] = match[2];
+    }else if(match = line.match(/^\[(.*)\]$/)){
+      section = result[match[1]] = {};
+    }else if( !/^\s*(;.*)?$/.test(line)){
+      throw new Error("Line '"+line+"'is not valid .")
+    }
+  })
+
+  return result;
+}
