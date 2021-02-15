bc55068f8d396fb9035ff3ca9b53f2f4e13fd204
HipsterSantos <santoscampos269@gmail.com>
2021-02-15 06:59:35 +0100

    Update exercises

--- languages/js/medium/rever-singlyLinkedlist.js
+++ languages/js/medium/rever-singlyLinkedlist.js
@@ -1,14 +1,1 @@
-function reverseSll(sll){
-  var node = sll.head;
-  var prev = null;
-  while(node){
-    var temp = node.next ;
-    node.next =prev;
-    prev = node;
-    if(!temp){
-      break;
-    }
-    node = temp;
-  }
-  return node;
-}
+//reverse a linkedlist
