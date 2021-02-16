8bfc0f24ce81a5d31e4a2105461452d0a260d466
HipsterSantos <santoscampos269@gmail.com>
2021-02-16 15:36:30 +0100

    last changes

--- languages/js/medium/addTwoNumbers.js
+++ languages/js/medium/addTwoNumbers.js
@@ -7,7 +7,6 @@ function SinglyLinkedList(){
   this.head = null;
   this.size = 0;
 }
-SinglyLinkedList.prototype.isEmpty = function(){
   return this.size == 0;
 }
 SinglyLinkedList.prototype.insert = function(value){
