67a9a9edcfb56dd03d3a86b94a209595ce0696e7
HipsterSantos <santoscampos269@gmail.com>
2021-02-17 07:09:20 +0100

    Doubly linked list

--- languages/js/medium/practice.js
+++ languages/js/medium/practice.js
@@ -1,1 +1,13 @@
-//this file is to help you redo what you've learned so far ,try coding everyting by coming from your mind 
+//this file is to help you redo what you've learned so far ,try coding everyting by coming from your mind
+function DoublyLinkedListNode(value){
+  this.data = value;
+  this.next = null;
+  this.prev = null;
+
+}
+
+function DoublyLinkedList(){
+  this.head  = null;
+  this.tail = null;
+  this.size = 0;
+}
