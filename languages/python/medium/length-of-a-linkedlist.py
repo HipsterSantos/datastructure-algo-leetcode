5fea3898271c3ff2923a67b4782549ff7e5cfa05
HipsterSantos <santoscampos269@gmail.com>
2021-02-19 06:58:09 +0100

    Length of  a LinkedList version two

--- languages/python/medium/length-of-a-linkedlist.py
+++ languages/python/medium/length-of-a-linkedlist.py
@@ -9,3 +9,17 @@ class Solution:
             f=f+1
             node = node.next
         return f
+# class LLNode:
+#     def __init__(self, val, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def solve(self, node):
+        f = 0
+        if node != None:
+            while node:
+                f=f+1
+                node = node.next
+            return f
+        else:
+            return f
