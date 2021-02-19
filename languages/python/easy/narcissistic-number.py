6ef339a78567b52094b86669705caadc273064e8
HipsterSantos <santoscampos269@gmail.com>
2021-02-19 07:32:25 +0100

    Narcissistic number

---
+++ languages/python/easy/narcissistic-number.py
@@ -0,0 +1,11 @@
+# Given an integer n, return whether it is equal to the sum of its own digits raised
+# to the power of the number of digits.
+
+class Solution:
+    def solve(self,x):
+        arr = list(x)
+        sum =len(x)-len(x)
+        for a in arr
+            sum  += a ** len(x)
+        return sum
+        
