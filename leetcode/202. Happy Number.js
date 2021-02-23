bef303f8f591790955df9cea435c1d02671f337c
HipsterSantos <santoscampos269@gmail.com>
2021-02-23 09:24:55 +0100

    javascript version happy number

--- leetcode/202. Happy Number.js
+++ leetcode/202. Happy Number.js
@@ -1,24 +1,21 @@
-class Solution:
-    def sum_happy(self,n):
-        sum = 0
-        while n is not 1:
-            digit = n % 10;
-            sum+= digit ** 2
-            n //= 10
-        return True
+var isHappy = function(n) {
+    const seenSet = new Set()
 
-c = Solution()
-print(c.sum_happy(2) )
+    const helper = (num) => {
+        if (num === 1) {
+            return true
+        }
 
-class Solution:
-    def solve(self,n):
-        sum =0
-        digit = 0
-        while n != 1:
-            digit = n//10
-            digit = digit **2
-            sum += digit
-            n = sum
-        print(digit)
-        print(sum)
-        return True
+        if (seenSet.has(num)) {
+            return false
+        }
+
+        seenSet.add(num)
+
+        const newNum = (num + '').split('').reduce((sum, digit) => sum + Math.pow(parseInt(digit), 2), 0)
+
+        return helper(newNum)
+    }
+
+    return helper(n)
+}
