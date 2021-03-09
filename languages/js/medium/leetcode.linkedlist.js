75cb476cb1cef85063b6a1438cfbd170768c0415
HipsterSantos <santoscampos269@gmail.com>
2021-03-09 06:47:32 +0100

    Test initialized , adding the constructor of linkedlist

--- languages/js/medium/leetcode.linkedlist.js
+++ languages/js/medium/leetcode.linkedlist.js
@@ -1,1 +1,12 @@
-//Design your linked List 
+//Design your linked List
+/**
+ * Initialize your data structure here.
+ */
+var Node = function(value){
+    this.data = value;
+    this.next = null;
+}
+var MyLinkedList = function() {
+    this.head = null;
+    this.size = 0;
+};
