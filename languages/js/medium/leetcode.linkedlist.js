f54b79ab75ed20d9659d60014b5fd59f52acf96e
HipsterSantos <santoscampos269@gmail.com>
2021-03-09 06:48:10 +0100

    get function

--- languages/js/medium/leetcode.linkedlist.js
+++ languages/js/medium/leetcode.linkedlist.js
@@ -10,3 +10,18 @@ var MyLinkedList = function() {
     this.head = null;
     this.size = 0;
 };
+
+/**
+ * Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+ * @param {number} index
+ * @return {number}
+ */
+MyLinkedList.prototype.get = function(index) {
+    var count = 0;
+    var curr = this.head;
+    while(count != index){
+        curr = curr.next;
+        count ++;
+    }
+    return curr;
+};
