b1fad10ac46a7d5a3ca8327f90a9c348aee843ad
HipsterSantos <santoscampos269@gmail.com>
2021-03-11 06:33:05 +0100

    leetcode solution

--- leetcode/reverse-integer.py
+++ leetcode/reverse-integer.py
@@ -24,3 +24,27 @@ Python Solution:
     def rever(x):
     res = -int(f'{x}'[:0:-1]) if x < 0 else int(f'{x}'[::-1])
     return res if -2**31 <= res <= 2**31-1 else 0
+
+Python Solution:
+    class Solution:
+    def reverse(self, x: int) -> int:
+        if x >= 0 and x <= 9:
+            return x
+        if x < 0 :
+            y = self.helper(x*-1)*-1
+        else :
+            y = self.helper(x)
+        if y <= (-2**31) or y >= (2**31)-1 :
+            return 0
+        else :
+            return y
+
+
+    def helper(self,x) :
+        y = ''
+        while x//10 != 0 :
+            y += str(x%10)
+            x = x//10
+            if x < 10 :
+                y += str(x)
+        return int(y)
