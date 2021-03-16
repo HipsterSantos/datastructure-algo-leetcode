af8c4a3b9f9e0fce40530b94624afacce5029c44
HipsterSantos <santoscampos269@gmail.com>
2021-03-16 08:36:48 +0100

    Remove duplicated

--- languages/js/medium/deleteduplicates.js
+++ languages/js/medium/deleteduplicates.js
@@ -1,34 +1,15 @@
-function deleteDuplicateInUnsortedSll(sll1){
-  var track = [];
-  var temp = sll1.head;
-  var prev= null;
-  while(temp){
-    if(track.indexOf(temp.data) >= 0){
-      prev.next  = temp.next;
-      sll1.size --;
-    }else{
-      track.push(temp.data);
-      prev = temp;
-    }
-    temp = temp.next;
-
-  }
-  console.log(temp);
-
-}
-
-// delete duplicate in unsoted linkedlist
-function deleteDuplicateInUnsortedSll(sll1){
+function removeDuplicates(sll){
   var track = {};
-  var  temp = sll1.head;
   var prev = null;
   while(temp){
-    if(track[temp.data]){
-      prev.next = temp.next ;
-      sll1.size --;
+    if(track.indexOf(temp.data) >=0 ){
+      prev.next = temp.next;
+      sll1.size--;
     }else{
-      track[tem.data] = true;
-      prev = temp;
+      track.push(temp.data);
+      prev = temp ;
     }
+    temp = temp.next;
   }
+  console.log(temp)
 }
