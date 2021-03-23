072bb0fb7101696af831856b20b23aa1cf0e4bf3
HipsterSantos <santoscampos269@gmail.com>
2021-03-23 13:49:11 +0100

    Ris file parser

---
+++ languages/js/easy/review/ris.js
@@ -0,0 +1,13 @@
+ris2.split(/([^]*?\n)\n/).filter(items =>items != '').forEach((line,index)=>{
+
+    line.split(/(.*\s*)-\s+/).forEach((line2,index)=>{
+    //    if( index % 2 == 0){
+    //   risObj.push({key:line2})
+    // }else{risObj.push({value:line2});}
+console.log(line2)
+
+});
+
+
+
+})
