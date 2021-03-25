702713bfccc7e8a08b79e5ee42cccba13e77704a
HipsterSantos <santoscampos269@gmail.com>
2021-03-25 19:41:44 +0100

    updating regular expression

--- languages/js/easy/review/ris.js
+++ languages/js/easy/review/ris.js
@@ -11,3 +11,33 @@ console.log(line2)
 
 
 })
+
+//ris
+
+ris2.split(/([^]*?\n)\n/).filter(items =>items != '').forEach((line,index)=>{
+    json = {};
+    line.split(/(.*\s*)-\s+/).forEach((line2,ind)=>{
+
+
+        if(ind % 2 == 0){
+            json['key'] = line2;
+        }else{
+            json['value'] = line2;
+            arr[index].push({...json})
+        }
+});
+
+console.log(arr)
+
+})
+
+//txt
+
+txt.split(/([^]*?\n)\n/).filter(items =>items != '').map((line)=>line).forEach( (line)=>{
+
+
+console.log(line.split(/(.*)*\s+([\n\w])/g))
+
+}
+
+)
